3a23d384fe12010f9512ba27fb97b13a8449cbf2
mauroapjr <mauroapjr@outlook.com>
2023-02-03 16:00:13 -0500

    Fix toggle form

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -58,17 +58,6 @@ $(document).ready(function() {
   // click to toggle form
   $(".form-toggle").on("click", toggleTweetForm);
 }); 
-// $(document).ready(function () {
-//   $("#tweet-text").on("input",function () {
-//     let maxChar = 140;
-//     $(".counter").val(maxChar - this.value.length);
 
-//     if (this.value.length >= 141) {
-//         $(".counter").addClass("red");
-//     } else {
-//         $(".counter").removeClass("red");
-//     }
-// });
-// });
 
 
