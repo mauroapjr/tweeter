3d11d2abe819583d0df99db4371aeb547bb6dc3a
mauroapjr <mauroapjr@outlook.com>
2023-02-03 15:20:36 -0500

    Add fixed tweet-hide

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -49,7 +49,6 @@ $(document).ready(() => {
   });
   
   const loadTweets = function () {
-    //console.log("consoleload");
     $.ajax("/tweets", { method: "GET" }).then(function (tweets) {
       renderTweets(tweets);
       console.log("Success", tweets);
@@ -59,61 +58,38 @@ $(document).ready(() => {
 
   const createTweetElement = (tweetData) => {
     const { user, content, created_at } = tweetData;
-
-    let singleTweetElement = $(`<article class="tweet-component">
-          <!-- image-username-refkey -->
-          <div class="image-username-refkey">
-            <div class="image-username">
-              <img src=${user.avatars} alt="" />
-              <span>${user.name}</span>
-            </div>
-            <div>${user.handle}</div>
-            </div>
-          <!-- tweet contect -->
-          <div class="tweet-content">
-            <!-- <p>${content.text}</p> -->
-            ${$("<p>").text(content.text).html()}
-          </div>
-          <!-- time and reactions icons -->
-          <div class="time-reactions">
-            <p>${timeago.format(created_at)}</p>
-            <div class="icons">
-              <i class="fas fa-flag"></i>
-              <i class="fas fa-retweet"></i>
-              <i class="fas fa-heart"></i>
-            </div>
-          </div>
-        </article>`);
-
+    let singleTweetElement = $(`
+    <article class="tweet-component">
+      <!-- image-username-refkey -->
+      <div class="image-username-refkey">
+        <div class="image-username">
+          <img src=${user.avatars} alt="" />
+          <span>${user.name}</span>
+        </div>
+        <div>${user.handle}</div>
+        </div>
+      <!-- tweet contect -->
+      <div class="tweet-content">
+        <!-- <p>${content.text}</p> -->
+        ${$("<p>").text(content.text).html()}
+      </div>
+      <!-- time and reactions icons -->
+      <div class="time-reactions">
+        <p>${timeago.format(created_at)}</p>
+        <div class="icons">
+          <i class="fas fa-flag"></i>
+          <i class="fas fa-retweet"></i>
+          <i class="fas fa-heart"></i>
+        </div>
+      </div>
+    </article>
+    `);
     return singleTweetElement;
   };
+  
   $(".new-tweet").hide();
-
-  // const createTweetElement = (tweet) => {
-  //   const safeHTML = escape(tweet.content.text);
-  //   const tweetOutput = `
-  //   <article class="tweet-received box" >
-  //     <header>
-  //       <div>
-  //         <img class="avatar" src=${tweet.user.avatars} />
-  //         <span class="tweeter-name">${tweet.user.name}</span>
-  //       </div>
-  //       <span class="new-user"> ${tweet.user.handle}</span>
-  //     </header>
-  //     <p class="text-received">${safeHTML}</p>
-  //     <footer>
-  //       <p class="created-date">${timeago.format(tweet.created_at)}</p>
-  //         <div class="icons">
-  //           <div class="flag"><i class="fas fa-flag"></i></div>
-  //           <div class="retweet"><i class="fas fa-retweet"></i></div>
-  //           <div class="like"><i class="fas fa-heart"></i></div>
-  //         </div>
-  //     </footer>
-  //   </article>
-  // `;
-  //   return tweetOutput;
-  // };
 });
+
 // toggle tweet form
 const toggleTweetForm = () => {
   var $section = $("section.new-tweet");
@@ -124,6 +100,7 @@ const toggleTweetForm = () => {
     $section.find("textarea").focus();
   }
 };
+
 // display errors
 const displayError = (err_msg) => {
   $("form").find(".error_msg").text(err_msg).show().slideDown("slow");
