75f5828eaa822d5b3b022fb571338a9ba8308c39
mauroapjr <mauroapjr@outlook.com>
2023-02-03 15:08:22 -0500

    Fix bugs after crashed

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,61 +1,89 @@
-$(document).ready(() => {
+let endpoint = "/tweets"; 
 
+$(document).ready(() => {
   const escape = function (str) {
     let div = document.createElement("div");
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   };
- 
+
   // loops through tweets
   // calls createTweetElement for each tweet
   // takes return value and appends it to the tweets container
 
-
   const renderTweets = function (tweets) {
     tweets.forEach((tweet) => {
       const $newTweet = createTweetElement(tweet);
-      
+
       //console.log("this is the new tweet", $newTweet);
       $("#tweets-container").append($newTweet);
     });
   };
 
-  $("#formData").on("submit", function (evt) {
-    evt.preventDefault();
-    
-    const formData = $(this).serialize();
-    const newPost = $("#tweet-text").val().length;
-    
-    if (!newPost) {
-      $('#error-empty').removeAttr("display");  
-      $('#error-empty').slideDown('slow');
-      
-    } else if (newPost > 140) {
-      $('#error-counter').removeAttr("display");  
-      $('#error-counter').slideDown('slow');
-          
-    } else {
-      $('#error-empty').slideUp();
-      $('#error-counter').slideUp();
-
-      $.ajax({
-        url: "/tweets",
-        type: "POST",
-        data: formData,
-        success: function (response) {
-          $("#tweet-text").val("");
-          loadTweets();
-          console.log("It sent");
-        },
-        error: function (error) {
-          console.log("error");
-        },
-      });
+  $("form").on("submit", function (e) {
+    e.preventDefault();
+    $(this).find("p").hide().slideDown("slow");
+    // if content is more than 140 characters
+    if ($(this).find("textarea").val().length > defaultCharsLeft) {
+      const error_msg = "You cannot exceed more than 140 characters";
+      return displayError(error_msg);
     }
+    // if content is empty
+    if ($(this).find("textarea").val().length < 1) {
+      const error_msg = "You cannot post an empty tweet.";
+      return displayError(error_msg);
+    }
+    $.ajax({
+      method: "POST",
+      url: endpoint,
+      type: "application/json",
+      data: $(this).serialize(),
+      success: function () {
+        $("textarea").val("");
+        $(".counter").val(0);
+        $(".error_msg").hide();
+        $.get("http://localhost:8080/tweets", (data) => {
+          const newTweet = [data.slice(-1).pop()];
+          renderTweets(newTweet);
+        });
+      },
+    });
   });
+  // $("#formData").on("submit", function (evt) {
+  //   evt.preventDefault();
+
+  //   const formData = $(this).serialize();
+  //   const newPost = $("#tweet-text").val().length;
+
+  //   if (!newPost) {
+  //     $('#error-empty').removeAttr("display");
+  //     $('#error-empty').slideDown('slow');
+
+  //   } else if (newPost > 140) {
+  //     $('#error-counter').removeAttr("display");
+  //     $('#error-counter').slideDown('slow');
+
+  //   } else {
+  //     $('#error-empty').slideUp();
+  //     $('#error-counter').slideUp();
+
+  //     $.ajax({
+  //       url: "/tweets",
+  //       type: "POST",
+  //       data: formData,
+  //       success: function (response) {
+  //         $("#tweet-text").val("");
+  //         loadTweets();
+  //         console.log("It sent");
+  //       },
+  //       error: function (error) {
+  //         console.log("error");
+  //       },
+  //     });
+  //   }
+  // });
 
   const loadTweets = function () {
-    
     //console.log("consoleload");
     $.ajax("/tweets", { method: "GET" }).then(function (tweets) {
       renderTweets(tweets);
@@ -64,30 +92,74 @@ $(document).ready(() => {
   };
   loadTweets();
 
-  const createTweetElement = (tweet) => {
-    const safeHTML = escape(tweet.content.text);
-    const tweetOutput = `
-    <article class="tweet-received box" > 
-      <header>
-        <div> 
-          <img class="avatar" src=${tweet.user.avatars} />
-          <span class="tweeter-name">${tweet.user.name}</span>
-        </div>  
-        <span class="new-user"> ${tweet.user.handle}</span>
-      </header>
-      <p class="text-received">${safeHTML}</p> 
-      <footer>
-        <p class="created-date">${timeago.format(tweet.created_at)}</p>
-          <div class="icons">
-            <div class="flag"><i class="fas fa-flag"></i></div>
-            <div class="retweet"><i class="fas fa-retweet"></i></div>
-            <div class="like"><i class="fas fa-heart"></i></div>  
+  const createTweetElement = (tweetData) => {
+    const { user, content, created_at } = tweetData;
+
+    let singleTweetElement = $(`<article class="tweet-component">
+          <!-- image-username-refkey -->
+          <div class="image-username-refkey">
+            <div class="image-username">
+              <img src=${user.avatars} alt="" />
+              <span>${user.name}</span>
+            </div>
+            <div>${user.handle}</div>
+            </div>
+          <!-- tweet contect -->
+          <div class="tweet-content">
+            <!-- <p>${content.text}</p> -->
+            ${$("<p>").text(content.text).html()}
           </div>
-      </footer>
-    </article>
-  `;
-    return tweetOutput;
+          <!-- time and reactions icons -->
+          <div class="time-reactions">
+            <p>${timeago.format(created_at)}</p>
+            <div class="icons">
+              <i class="fas fa-flag"></i>
+              <i class="fas fa-retweet"></i>
+              <i class="fas fa-heart"></i>
+            </div>
+          </div>
+        </article>`);
+
+    return singleTweetElement;
   };
+  $(".new-tweet").hide();
 
+  // const createTweetElement = (tweet) => {
+  //   const safeHTML = escape(tweet.content.text);
+  //   const tweetOutput = `
+  //   <article class="tweet-received box" >
+  //     <header>
+  //       <div>
+  //         <img class="avatar" src=${tweet.user.avatars} />
+  //         <span class="tweeter-name">${tweet.user.name}</span>
+  //       </div>
+  //       <span class="new-user"> ${tweet.user.handle}</span>
+  //     </header>
+  //     <p class="text-received">${safeHTML}</p>
+  //     <footer>
+  //       <p class="created-date">${timeago.format(tweet.created_at)}</p>
+  //         <div class="icons">
+  //           <div class="flag"><i class="fas fa-flag"></i></div>
+  //           <div class="retweet"><i class="fas fa-retweet"></i></div>
+  //           <div class="like"><i class="fas fa-heart"></i></div>
+  //         </div>
+  //     </footer>
+  //   </article>
+  // `;
+  //   return tweetOutput;
+  // };
 });
-
+// toggle tweet form
+const toggleTweetForm = () => {
+  var $section = $("section.new-tweet");
+  if ($section.is(":visible")) {
+    $section.slideUp("fast");
+  } else {
+    $section.slideDown("fast");
+    $section.find("textarea").focus();
+  }
+};
+// display errors
+const displayError = (err_msg) => {
+  $("form").find(".error_msg").text(err_msg).show().slideDown("slow");
+};
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,74 @@
 // function to count characters of tweet
-$(document).ready(function () {
-  $("#tweet-text").on("input",function () {
-    let maxChar = 140;
-    $(".counter").val(maxChar - this.value.length);
+let prevLengthOfTextarea = 0;
+let defaultCharsLeft = 140;
 
-    if (this.value.length >= 141) {
-        $(".counter").addClass("red");
-    } else {
-        $(".counter").removeClass("red");
-    }
-});
-});
+// check scroll by user
+const toggleBackTpTopBtn = () => {
+  if ($(window).scrollTop() > 0) {
+    $(".back-to-top")
+      .show()
+      .fadeIn("slow");
+  } else {
+    $(".back-to-top")
+      .hide()
+      .fadeOut("slow");
+  }
+};
+
+$(document).ready(function() {
+  $("form")
+    .find("textarea")
+    .keyup(function() {
+      const charInTextarea = $(this).val().length;
+      const charsLeft = defaultCharsLeft - charInTextarea;
+      if (charInTextarea > defaultCharsLeft) {
+        ["-", charsLeft].join("");
+        $(this)
+          .next()
+          .find("output")
+          .text(charsLeft)
+          .css("color", "red");
+      } else {
+        $(this)
+          .next()
+          .find("output")
+          .text(charsLeft)
+          .css("color", "black");
+      }
+      $(this)
+        .next()
+        .find("output")
+        .text(charsLeft);
+      
+    });
+
+  // hide back to top button at initial render
+  $(".back-to-top").hide();
+  // call toggleBackTpTopBtn on page scroll
+  $(window).scroll(toggleBackTpTopBtn);
+  // clicking on back to top
+  $(".back-to-top").on("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+    toggleBackTpTopBtn();
+  });
+
+  // click to toggle form
+  $(".form-toggle").on("click", toggleTweetForm);
+}); 
+// $(document).ready(function () {
+//   $("#tweet-text").on("input",function () {
+//     let maxChar = 140;
+//     $(".counter").val(maxChar - this.value.length);
+
+//     if (this.value.length >= 141) {
+//         $(".counter").addClass("red");
+//     } else {
+//         $(".counter").removeClass("red");
+//     }
+// });
+// });
 
 
